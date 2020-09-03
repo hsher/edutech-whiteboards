59a4e1e1a6f8c4a8edfd14ad3bd58b2ccd03ae18
Yurii Mochalov <ym@anahoret.com>
2020-09-03 15:02:49 +0300

    Add base console structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,69 @@ import './App.scss';
 function App() {
   return (
     <>
+      <div className="Console">
+        <ul>
+          <li>
+            <button className="Console-button">
+              <img src="asd" />
+
+              <span>
+                CURSOR
+              </span>
+            </button>
+
+            <ul>
+              <li>
+                <button className="Console-button">
+                  <img src="asd" />
+
+                  <span>
+                    PHOTO
+                  </span>
+                </button>
+
+                <button className="Console-button">
+                  <img src="asd" />
+
+                  <span>
+                    ERASE
+                  </span>
+                </button>
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <button className="Console-button">
+              <img src="asd" />
+
+              <span>
+                TYPE
+              </span>
+            </button>
+          </li>
+
+          <li>
+            <button className="Console-button">
+              <img src="asd" />
+
+              <span>
+                DRAW
+              </span>
+            </button>
+          </li>
+
+          <li>
+            <button className="Console-button">
+              <img src="asd" />
+
+              <span>
+                BOARD
+              </span>
+            </button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
